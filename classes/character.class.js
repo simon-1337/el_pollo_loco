5b6511529695c4269b66df0b312ebb2dbc85cbfa
simon-1337 <simon.besenbaeck@gmail.com>
2023-02-08 09:14:46 +0100

    sound

--- classes/character.class.js
+++ classes/character.class.js
@@ -12,11 +12,12 @@ class Character extends MoveableObject {
         'img/2_character_pepe/2_walk/W-26.png'
     ];
     world;
+    walking_sound = new Audio('audio/walking.mp3');
 
-    
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
+        this.walking_sound.playbackRate = 2.25;
         this.loadImages(this.IMAGES_WALKING);
         this.animate();
     }
@@ -28,14 +29,17 @@ class Character extends MoveableObject {
     animate() {
 
         setInterval(() => {
+            this.walking_sound.pause();
             if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.x += this.speed;
                 this.otherDirection = false;
+                this.walking_sound.play();
             }
 
             if (this.world.keyboard.LEFT && this.x > -719) {
                 this.x -= this.speed;
                 this.otherDirection = true;
+                this.walking_sound.play();
             }
             this.world.camera_x = -this.x + 100;
         }, 1000 / 60)
